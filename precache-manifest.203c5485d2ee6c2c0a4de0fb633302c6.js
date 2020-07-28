987ca22c354e346aeb68bed5ddc39e5f5bf8c5bf
AndreyChenzov242 <andreychenzov@gmail.com>
2020-07-28 21:34:25 +0300

    Updates

---
+++ precache-manifest.203c5485d2ee6c2c0a4de0fb633302c6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1184e0223ba6d90e78334532b39fdb5f",
+    "url": "/My-first-react-app/index.html"
+  },
+  {
+    "revision": "4df7048a20b00449c8c6",
+    "url": "/My-first-react-app/static/css/2.e08a537e.chunk.css"
+  },
+  {
+    "revision": "07d060fd4bca08b9acbc",
+    "url": "/My-first-react-app/static/css/main.cb16e8cc.chunk.css"
+  },
+  {
+    "revision": "4df7048a20b00449c8c6",
+    "url": "/My-first-react-app/static/js/2.bef27a11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/My-first-react-app/static/js/2.bef27a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d060fd4bca08b9acbc",
+    "url": "/My-first-react-app/static/js/main.fa9096fc.chunk.js"
+  },
+  {
+    "revision": "65728730e86b97afdc99",
+    "url": "/My-first-react-app/static/js/runtime-main.b33c6b65.js"
+  },
+  {
+    "revision": "2c966e92cd0a88abb56b890f6a4817ae",
+    "url": "/My-first-react-app/static/media/home-slider-img-1.2c966e92.png"
+  },
+  {
+    "revision": "fff0a4dc745d9284bb253f21537540d7",
+    "url": "/My-first-react-app/static/media/main-bg.fff0a4dc.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/My-first-react-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/My-first-react-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/My-first-react-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/My-first-react-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
